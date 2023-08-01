d965f8880333c1f43bddd747ea41d6b9be767f5e
deking254 <dekingsky@gmail.com>
2023-08-01 08:37:21 +0000

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,8 +1,14 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_insert_left - Entry point
+ * @parent: parent
+ * @value: val
+ * Return: Always 0 (Success)
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 binary_tree_t *child;
-if (parent != NULL)
+if (parent)
 {
 child = malloc(sizeof(binary_tree_t));
 if (child == NULL)
@@ -10,8 +16,8 @@ return (NULL);
 if (parent->left)
 {
 child = binary_tree_node(parent, value);
-parent->left->parent = child;
 child->left = parent->left;
+parent->left->parent = child;
 parent->left = child;
 }
 else
