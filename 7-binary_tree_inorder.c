902a601aa188f8425658086956034f40c89e8c0d
deking254 <dekingsky@gmail.com>
2023-08-01 12:33:10 +0000

    7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,1 +1,22 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_inorder - Entry point
+ * @tree: root
+ * @func: func
+ * Return: Always 0 (Success)
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree)
+{
+if (func)
+{
+/*if (tree->parent == NULL)
+func(tree->n);*/
+binary_tree_inorder(tree->left, func);
+if (tree)
+func(tree->n);
+binary_tree_inorder(tree->right, func);
+}
+}
+}
