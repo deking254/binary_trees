19b35770ec1228c9d1cabed08f3df28e46f2b61a
deking254 <dekingsky@gmail.com>
2023-08-01 10:49:37 +0000

    6

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,1 +1,21 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - Entry point
+ * @tree: root
+ * @func: func
+ * Return: Always 0 (Success)
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree)
+{
+if (tree->parent == NULL)
+func(tree->n);
+if (tree->left)
+func(tree->left->n);
+binary_tree_preorder(tree->left, func);
+if (tree->right)
+func(tree->right->n);
+binary_tree_preorder(tree->right, func);
+}
+}
