f58ef353b30a314faf5220354cce3ffe85a875e8
deking254 <dekingsky@gmail.com>
2023-08-01 08:45:05 +0000

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 binary_tree_t *child;
 if (parent)
 {
-child = malloc(sizeof(binary_tree_t));
-if (child == NULL)
-return (NULL);
 if (parent->left)
 {
 child = binary_tree_node(parent, value);
