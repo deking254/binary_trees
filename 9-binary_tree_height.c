6b80a34c76f5ba58f5866995c03bfdb63e3096b6
deking254 <dekingsky@gmail.com>
2023-08-01 14:14:46 +0000

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,1 +1,18 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_height - Entry point
+ * @tree: root
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+if (tree == NULL)
+return (-1);
+else
+{
+size_t a = binary_tree_height(tree->left);
+size_t b = binary_tree_height(tree->right);
+return (1 + ((a > b) ? b : a));
+}
+return (0);
+}
