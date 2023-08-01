a50c2fa6de881ab1a5a29a0702ef1af43e704bde
deking254 <dekingsky@gmail.com>
2023-08-01 15:10:57 +0000

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,13 +6,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t y = 0;
 if (tree == NULL)
-return (-1);
+return (y);
+if (tree->left == NULL && tree->right == NULL)
+return (y);
 else
 {
 size_t a = binary_tree_height(tree->left);
 size_t b = binary_tree_height(tree->right);
-return (1 + ((a >= b) ? b : a));
+return (1 + ((a > b) ? b : a));
 }
 return (1);
 }
