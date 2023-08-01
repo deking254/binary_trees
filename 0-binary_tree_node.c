fc80c0590008d6bbce0fe828db2b8289b2cd63ac
deking254 <dekingsky@gmail.com>
2023-08-01 07:17:16 +0000

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,10 +3,13 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *node;
 node = malloc(sizeof(binary_tree_t));
-if (node != NULL)
+if (node)
+{
+if (parent)
 {
 node->n = value;
 node->parent = parent;
+}
 }
 else
 return (NULL);
