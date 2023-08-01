0a482e7969e53dbdfd35aedb48495d09d564eb8e
deking254 <dekingsky@gmail.com>
2023-08-01 09:50:12 +0000

    4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,1 +1,19 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - Entry point
+ * @node: nodeleaf
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+if (node)
+{
+if (node->left)
+return (0);
+if (node->right)
+return (0);
+}
+else
+return (0);
+return (1);
+}
