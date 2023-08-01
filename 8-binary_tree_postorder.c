b556280bd03d88ce79dad897049e260c2e4f955b
deking254 <dekingsky@gmail.com>
2023-08-01 12:46:31 +0000

    8

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,1 +1,24 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Entry point
+ * @tree: root
+ * @func: func
+ * Return: Always 0 (Success)
+ */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree)
+{
+if (func)
+{
+binary_tree_postorder(tree->left, func);
+if (tree->left)
+func(tree->left->n);
+binary_tree_postorder(tree->right, func);
+if (tree->right)
+func(tree->right->n);
+if (tree->parent == NULL)
+func(tree->n);
+}
+}
+}
