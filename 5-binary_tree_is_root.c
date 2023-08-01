0d11903665b0aea09a093b6162f3ef39b34f2a4d
deking254 <dekingsky@gmail.com>
2023-08-01 09:54:19 +0000

    5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,1 +1,17 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - Entry point
+ * @node: node root
+ * Return: Always 0 (Success)
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+if (node)
+{
+if (node->parent)
+return (0);
+}
+else
+return (0);
+return (1);
+}
