f8a2c9db0693b5a1a90b5470aa5eb61b846587ae
deking254 <dekingsky@gmail.com>
2023-08-01 15:24:31 +0000

    10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,1 +1,19 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - Entry point
+ * @tree: root
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+size_t y = 0;
+if (tree)
+{
+if (tree->parent)
+{
+size_t a = binary_tree_depth(tree->parent);
+return (1 + (a));
+}
+}
+return (y);
+}
