649468eb09df7f4dbc4dc2528b73477edc85960c
deking254 <dekingsky@gmail.com>
2023-08-01 15:32:13 +0000

    11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,1 +1,17 @@
-d
+#include "binary_trees.h"
+/**
+ * binary_tree_size - Entry point
+ * @tree: root
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+size_t y = 0;
+if (tree)
+{
+size_t a = binary_tree_size(tree->left);
+size_t b = binary_tree_size(tree->right);
+return (1 + (a + b));
+}
+return (y);
+}
