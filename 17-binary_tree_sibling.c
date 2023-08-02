24f5ee1b62329851954ec000c2aa8ae4f690e975
deking254 <dekingsky@gmail.com>
2023-08-02 16:30:55 +0000

    17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -6,5 +6,21 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-
+if (node)
+{
+if (node->parent)
+{
+if (node->parent->left == node)
+{
+if (node->parent->right)
+return (node->parent->right);
+}
+if (node->parent->right == node)
+{
+if (node->parent->left)
+return (node->parent->left);
+}
+}
+}
+return (NULL);
 }
