21f838447cdcb215dec3920b4f4f06ced8988e21
deking254 <dekingsky@gmail.com>
2023-08-02 10:22:51 +0000

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+size_t binary_tree_height(const binary_tree_t *tree);
 /**
  * binary_tree_balance - Entry point
  * @tree: root
@@ -17,4 +18,25 @@ b++;
 return (a - b);
 }
 return (0);
+}
+
+/**
+ * binary_tree_height - Entry point
+ * @tree: root
+ * Return: Always 0 (Success)
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t y = 0;
+if (tree == NULL)
+return (y);
+if (tree->left == NULL && tree->right == NULL)
+return (y);
+else
+{
+size_t a = binary_tree_height(tree->left);
+size_t b = binary_tree_height(tree->right);
+return (1 + ((a > b) ? a : b));
+}
+return (1);
 }
