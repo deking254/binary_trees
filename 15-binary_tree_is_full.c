9677158f409b8af0d1a907008445f58774cb9fb4
deking254 <dekingsky@gmail.com>
2023-08-02 12:28:18 +0000

    15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -6,5 +6,16 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-
+if (tree)
+{
+if (tree->left == NULL || tree->right == NULL)
+return (0);
+}
+if (tree)
+{
+size_t a = binary_tree_is_full(tree->left);
+size_t b = binary_tree_is_full(tree->right);
+return ((a == b) ? 1 : 0);
+}
+return (0);
 }
