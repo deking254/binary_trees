5eea623d6f519586fa87904e74ad1e966d519cd6
deking254 <dekingsky@gmail.com>
2023-08-02 06:18:28 +0000

    14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,5 +6,18 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-
+if (tree)
+{
+if (tree->left  && tree->right)
+return (2);
+if (tree->left == NULL && tree->right == NULL)
+return (0);
+}
+if (tree)
+{
+size_t a = binary_tree_balance(tree->left);
+size_t b = binary_tree_balance(tree->right);
+return (-1 + (a + b));
+}
+return (0);
 }
