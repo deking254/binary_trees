ae45900e2cbbf6c4f10d92200764f24fad3799d9
deking254 <dekingsky@gmail.com>
2023-08-04 14:19:49 +0000

    101

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -7,5 +7,20 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+int forward = 0;
+int back = 0;
+if (tree)
+{
+binary_tree_t *nodes[100];
+nodes[back++] = (binary_tree_t *)tree;
+while (forward < back)
+{
+binary_tree_t *current = nodes[forward++];
+func(current->n);
+if (current->left)
+nodes[back++] = current->left;
+if (current->right)
+nodes[back++] = current->right;
+}
+}
 }
