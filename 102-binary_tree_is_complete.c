ac7dd1b83e99406c4ac0257259d0f4a458488f77
deking254 <dekingsky@gmail.com>
2023-08-05 09:57:47 +0000

    102

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -6,5 +6,33 @@
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-
+int forward = 0;
+int back = 0;
+if (tree)
+{
+binary_tree_t *nodes[100];
+nodes[back++] = (binary_tree_t *)tree;
+int null_present = 0;
+while (forward < back)
+{
+binary_tree_t *current = nodes[forward++];
+if (current->left)
+{
+if (null_present)
+return (0);
+nodes[back++] = current->left;
+}
+else
+null_present = 1;
+if (current->right)
+{
+if (null_present)
+return (0);
+nodes[back++] = current->right;
+}
+else
+null_present = 1;
+}
+}
+return (1);
 }
